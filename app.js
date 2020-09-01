089dde138f53a176f597259ad26da42c663e7977
lyndenlim <65570977+lyndenlim@users.noreply.github.com>
2020-09-01 13:21:07 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const grailedFee = 0.09
+const paypalFeePercent = 0.029
+const paypalFeeFlat = 0.03
+
+
+$(".feeButton").on("click", function(){
+    var results = (calculate(Number($(".salePrice").val()), Number($(".shippingCharge").val()), Number($(".shippingCost").val())))
+    if(results[2] !== 0){
+        $(".grailedFee").text(`$${results[0].toFixed(2)}`)
+        $(".paypalFee").text(`$${results[1].toFixed(2)}`)
+        $(".total").text(`$${results[2].toFixed(2)}`);
+    } else {
+        alert("Please enter a number greater than 0.")
+    }
+})
+
+function calculate(salePrice, shippingCharge, shippingCost){
+    var totalGrailedFee = salePrice * grailedFee
+    var totalPaypalFee = salePrice * paypalFeePercent + paypalFeeFlat
+    var totalFee = totalGrailedFee + totalPaypalFee - shippingCharge + shippingCost
+    var received = salePrice - totalFee
+    return [totalGrailedFee, totalPaypalFee, received]
+}
